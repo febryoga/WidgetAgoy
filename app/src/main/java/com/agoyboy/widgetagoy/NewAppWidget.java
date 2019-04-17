56e0b8fc9384d566e97f3257510be86f084eee45
febryoga <febryogasaputra@gmail.com>
2019-04-17 21:47:38 +0700

    beta

---
+++ app/src/main/java/com/agoyboy/widgetagoy/NewAppWidget.java
@@ -0,0 +1,51 @@
+package com.agoyboy.widgetagoy;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+    private static final String mSharedPrefFile =
+            "com.example.android.appwidgetsample";
+    private static final String COUNT_KEY = "count";
+    String dateString =
+            DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(
+                mSharedPrefFile, 0);
+        int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
+        count++;
+
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+        }
+
+    }
+
+}
+
